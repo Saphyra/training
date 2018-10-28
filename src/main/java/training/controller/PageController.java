9c53cedd7bcf7bbac3d2e5e195b84d47bfb08585
Bernat Bonda <bondabear@gmail.com>
2018-10-28 23:37:29 +0100

    Sources page

--- src/main/java/training/controller/PageController.java
+++ src/main/java/training/controller/PageController.java
@@ -1,25 +1,29 @@
 package training.controller;
 
-import java.io.IOException;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import lombok.extern.slf4j.Slf4j;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Controller
 @Slf4j
 public class PageController {
     private static final String HOME_MAPPING = "/";
+    private static final String SOURCE_MAPPING = "/source";
     private static final String PAGE_MAPPING = "book/{bookName}/{pageNumber}";
 
     @GetMapping(HOME_MAPPING)
     public String index() {
         return "index";
+    }
+
+    @GetMapping(SOURCE_MAPPING)
+    public String source(){
+        return "source";
     }
 
     @GetMapping(path = PAGE_MAPPING, produces = "text/html")
